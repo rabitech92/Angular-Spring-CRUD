b5d78cc5a98c8b50576bb6388c3d1f0bec69bc98
Rabiul Islam <rabiulnewsinfo@gmail.com>
2025-06-13 15:43:25 +0600

    first project initialization

---
+++ angular-spring/angular-spring/src/test/java/com/angular/spring/AngularSpringCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.angular.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AngularSpringCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
