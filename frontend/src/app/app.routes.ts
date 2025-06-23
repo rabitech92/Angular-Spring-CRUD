69577355cacd6c8e0508ad0008eec5e367f96afe
rabiul islam <rabiulnewsinfo@gmail.com>
2025-06-23 17:54:39 +0600

    forn end deleted

--- frontend/src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { UserComponent } from './component/user-component/user-component';
-
-export const routes: Routes = [
-  {path : '', component: UserComponent}
-
-
-];
--- frontend/src/app/model/user/user-info.model.ts
+++
@@ -1,5 +0,0 @@
-export interface UserInfo {
-  userName: string;
-  email: string;
-  password?: string;
-}
