38c9876afc389d89a6487050519625e6e9fa5050
rabiul <rabiulnewsinfo@gmail.com>
2025-06-27 00:59:29 +0600

    project start

---
+++ forntend/src/app/app-module.ts
@@ -0,0 +1,26 @@
+import { NgModule, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { BrowserModule, provideClientHydration, withEventReplay } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing-module';
+import { App } from './app';
+import { UserCreate } from './user-create/user-create';
+import { UserList } from './component/user-list/user-list';
+
+@NgModule({
+  declarations: [
+    App,
+    UserCreate,
+    UserList
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideZonelessChangeDetection(),
+    provideClientHydration(withEventReplay())
+  ],
+  bootstrap: [App]
+})
+export class AppModule { }
